04b1fe8d980da921cac19ee6d7e6f2fd6496b9d8
tomas.pacheco <tomas.p@cheil.com>
2023-06-05 17:44:57 -0400

    added sass and add navbar comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import './App.scss';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="container">
       <Navbar/>
+      <div>
+        <h1>Lorem Ipsum</h1>
+        <p>Lorem Ipsum</p>
+        <a href='#' target='_blank' className='text-uppercase cta-btn'>contact me</a>
+      </div>
     </div>
   );
 }
