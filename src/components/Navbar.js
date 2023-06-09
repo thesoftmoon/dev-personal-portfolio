931150dddd1dcc0aa21de3aeba552de3363706de
Tomás <tomas.pacheco.c@gmail.com>
2023-06-09 14:28:44 -0400

    mo navbar add, various responsiveness fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,26 @@
 import React, { useState } from "react";
 
 function Navbar() {
-
-  const navBarPc = document.querySelector('.navbar-pc');
-
   const [navBgColor, setNavBgColor] = useState(false);
 
-  const navbarBgChange = ()=>{
-    if(window.scrollY >= 90){
-      console.log('more than');
-      setNavBgColor(true);      
+  const navbarBgChange = () => {
+    if (window.scrollY >= 90) {
+      console.log("more than");
+      setNavBgColor(true);
     } else {
-      console.log('not scrolling');
+      console.log("not scrolling");
       setNavBgColor(false);
     }
   };
 
-  window.addEventListener('scroll', navbarBgChange);
+  window.addEventListener("scroll", navbarBgChange);
 
+  const [isOpened, setIsOpened] = useState(false);
+  const handleClick = () => {
+    setIsOpened(!isOpened);
+    console.log("clicked the menu");
+  };
 
-  console.log('working');
   return (
     <div className={navBgColor ? "navbar-pc navbar-background" : "navbar-pc"}>
       <div className="container">
@@ -45,9 +46,41 @@ function Navbar() {
                 fill="black"
               />
             </g>
-            
           </svg>
         </div>
+        <div
+          className={isOpened ? "navbar-links-mo worked" : "navbar-links-mo"}
+        >
+          <button
+            class={isOpened ? "opened menu" : "menu"}
+            onClick={handleClick}
+            aria-label={isOpened}
+          >
+            <svg width="60" height="60" viewBox="0 0 100 100">
+              <path
+                class="line line1"
+                d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+              />
+              <path class="line line2" d="M 20,50 H 80" />
+              <path
+                class="line line3"
+                d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+              />
+            </svg>
+          </button>
+          <div className={isOpened ? "links-mo" : "d-none"}>
+            <a className="navbar-mo-btn" href="#whatido">
+              <h2>what i do?</h2>
+            </a>
+            <a className="navbar-mo-btn" href="#projects">
+              <h2>projects</h2>
+            </a>
+            <a className="navbar-mo-btn" href="#contact">
+              <h2>contact</h2>
+            </a>
+          </div>
+        </div>
+
         <div className="navbar-links-pc d-none d-md-flex">
           <a className="navbar-btn" href="#whatido">
             what i do?
